bc6517c7c8353e15cb07c5c8da63b7a9e73ebb44
Joaquín Frías <119880493+Joaquin1245@users.noreply.github.com>
2023-03-03 09:19:41 -0300

    Termino el módulo Hard & Soft skills

--- ProyectoIntegrador_JoaquinFrias/src/app/app.module.ts
+++ ProyectoIntegrador_JoaquinFrias/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HysComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
